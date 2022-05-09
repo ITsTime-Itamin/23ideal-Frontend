55f3fe1c0ff7eefe354f3a96a806b2144e4f83f8
Yesol <violet010323@naver.com>
2022-05-09 22:27:14 +0900

    Menu Done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,56 +1,53 @@
-import { Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./Navbar.css"
 
-const Navbar=()=>{
-    return(
+const Navbar = () => {
+    return (
         <nav className="navbar">
-            <hr/>
-            <div className="login-bar" style={{float: "right", marginRight:"20px"}}>
-                <Link to="/Login" style={{textDecoration:"none", color:"black", marginRight:"10px"}}> Login </Link>
+            <hr />
+            <div className="login-bar" style={{ float: "right", marginRight: "20px" }}>
+                <Link to="/Login" style={{ textDecoration: "none", color: "black", marginRight: "10px" }}> Login </Link>
                 |
-                <Link to="/" style={{textDecoration:"none", color:"black", marginLeft:"7px"}}> 회원가입</Link>
+                <Link to="/" style={{ textDecoration: "none", color: "black", marginLeft: "7px" }}> 회원가입</Link>
             </div>
             <Link className="nav-logo" to="/"> Logo </Link>
             <ul>
                 <li className="nav-item">
-                    <Link  className="nav-link" to="/">모집공고 찾기</Link>
-                    <hr style={{border:"none", border:"0.05px solid #EB7E5D"}}/>
+                    <Link className="nav-link" to="/Find_All">모집공고 찾기</Link>
+                    <hr style={{ border: "none", border: "0.05px solid #EB7E5D" }} />
                     <div className="dropdown-content">
                         <li>
-                            <Link to="/Community" className="dropdown-item">모집공고 모두 보기</Link>
+                            <Link to="/FindAll" className="dropdown-item">모집공고 모두 보기</Link>
                         </li>
                         <li>
-                            <Link to="/Community" className="dropdown-item">내 조건에 맞는 공고 찾기</Link>
-                        </li> 
-                        <li>
-                            <Link to="/Community" className="dropdown-item">청년주택 공고</Link>
-                        </li> 
-                        <li>
-                            <Link to="/Community" className="dropdown-item">행복주택 공고</Link>
-                        </li> 
-                        <li>
-                            <Link to="/Community" className="dropdown-item">임대주택 공고</Link>
-                        </li> 
-                    </div>
-                </li>
-                <li className="nav-item">
-                    <Link  className="nav-link" to="/">청년지원정책</Link>
-                    <hr style={{border:"none", border:"0.05px solid #EB7E5D"}}/>
-                    <div className="dropdown-content">
-                        <li>
-                            <Link to="/Community" className="dropdown-item">정부의 보증금 지원 정책</Link>
+                            <Link to="/FindYoung" className="dropdown-item">청년주택 공고</Link>
                         </li>
                         <li>
-                            <Link to="/Community" className="dropdown-item">대출 관련 지원 정책</Link>
+                            <Link to="/FindHappy" className="dropdown-item">행복주택 공고</Link>
                         </li>
                         <li>
-                            <Link to="/Community" className="dropdown-item">은행별 대출 관련 서비스</Link>
+                            <Link to="/FindRent" className="dropdown-item">임대주택 공고</Link>
                         </li>
                     </div>
                 </li>
                 <li className="nav-item">
-                    <Link  className="nav-link" to="/">맞춤형 서비스</Link>
-                    <hr style={{border:"none", border:"0.05px solid #EB7E5D"}}/>
+                    <Link className="nav-link" to="/SupportGov">청년지원정책</Link>
+                    <hr style={{ border: "none", border: "0.05px solid #EB7E5D" }} />
+                    <div className="dropdown-content">
+                        <li>
+                            <Link to="/SupportGov" className="dropdown-item">정부의 보증금 지원 정책</Link>
+                        </li>
+                        <li>
+                            <Link to="SupportLoan" className="dropdown-item">대출 관련 지원 정책</Link>
+                        </li>
+                        <li>
+                            <Link to="SupportBank" className="dropdown-item">은행별 대출 관련 서비스</Link>
+                        </li>
+                    </div>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to="/Customized">맞춤형 서비스</Link>
+                    <hr style={{ border: "none", border: "0.05px solid #EB7E5D" }} />
                     <div className="dropdown-content">
                         <li>
                             <Link to="/Community" className="dropdown-item">내 청약가점 계산하기</Link>
@@ -58,20 +55,20 @@ const Navbar=()=>{
                     </div>
                 </li>
                 <li className="nav-item">
-                    <Link className="nav-link"  to="/Community">커뮤니티</Link>
-                    <hr style={{border:"none", border:"0.05px solid #EB7E5D"}}/>
+                    <Link className="nav-link" to="/Community">커뮤니티</Link>
+                    <hr style={{ border: "none", border: "0.05px solid #EB7E5D" }} />
                     <div className="dropdown-content">
                         <li>
-                            <Link to="/Community" className="dropdown-item">공지사항</Link>
+                            <Link to="/CommunityNotice" className="dropdown-item">공지사항</Link>
                         </li>
                         <li>
-                            <Link to="/Community" className="dropdown-item">입주 및 당첨후기</Link>
+                            <Link to="/CommunityCom" className="dropdown-item">입주 및 당첨후기</Link>
                         </li>
                         <li>
-                            <Link to="/Community" className="dropdown-item">자유게시판</Link>
+                            <Link to="/CommunityFree" className="dropdown-item">자유게시판</Link>
                         </li>
                         <li>
-                            <Link to="/Community" className="dropdown-item">취업 공고</Link>
+                            <Link to="/CommunityJob" className="dropdown-item">취업 공고</Link>
                         </li>
                     </div>
                 </li>
