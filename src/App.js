dfabf2f30ff54ca2f01528f9967820c3f1920e6a
chaeyeonan <dkscodus1204@ewhain.net>
2022-05-13 17:36:31 +0900

    chaeyeon: modify MAP API Kakao -> Naver

--- src/App.js
+++ src/App.js
@@ -2,8 +2,16 @@ import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Login from "./components/Login.js";
+import { useState } from "react";
+import axios from "axios";
 
 const App=()=>{
+
+  const [data,setData]=useState(null);
+  const onClick=()=>{
+    axios.get('http://localhost:8080/api/v1/houses').then(response=>{setData(response.data);});
+  };
+
   return(
     <div>
       <Navbar/>
@@ -11,6 +19,11 @@ const App=()=>{
         <Route path="/" element={<Home />}/>
         <Route path="/Login" element={<Login />}/>
       </Routes>
+
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      {data&&<textarea rows={7} value={JSON.stringify(data,null,2)} readOnly={true} />}
     </div>
   );
 };
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,116 +1,38 @@
-import React, { useEffect } from "react";
-import './Home.css';
-const Home = () => {
+import { RenderAfterNavermapsLoaded, NaverMap } from 'react-naver-maps';
+import React from 'react';
 
 
-    const new_script = src => {
-        return new Promise((resolve, reject) => {
-            const script = document.createElement('script');
-            script.src = src;
-            script.addEventListener('load', () => {
-                resolve();
-            });
-            script.addEventListener('error', e => {
-                reject(e);
-            });
-            document.head.appendChild(script);
-        });
-    };
+function NaverMapAPI() {
+  return (
+    <NaverMap
+      mapDivId={'maps-getting-started-uncontrolled'} // default: react-naver-map
+      style={{
+        width: '80%',
+        height: '600px',
+        alignItems: 'center',
+        justifyContent: 'auto',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        borderStyle: 'solid',
+        borderWidth: 'medium',
+        borderColor: '#D8D8D8'
+      }}
+      defaultCenter={{ lat: 37.554722, lng: 126.970833 }} // 지도 초기 위치
+      defaultZoom={13} // 지도 초기 확대 배율
+    />
+  );
+}
 
-    useEffect(() => {
-        const my_script = new_script("https://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=658194b4f2b0f7fea8c9097f1a8b6b49");
+function Home() {
+  return (
+    <RenderAfterNavermapsLoaded
+      ncpClientId={'optd2y01e0'} // 자신의 네이버 계정에서 발급받은 Client ID
+      error={<p>Maps Load Error</p>}
+      loading={<p>Maps Loading...</p>}
+    >
+      <NaverMapAPI />
+    </RenderAfterNavermapsLoaded>
+  );
+}
 
-        my_script.then(() => {
-            console.log('script loaded!!!');
-            const kakao = window['kakao'];
-            kakao.maps.load(() => {
-                const mapContainer = document.getElementById('map');
-                const options = {
-                    center: new kakao.maps.LatLng(37.56000302825312, 126.97540593203321),
-                    level: 3
-                };
-                const map = new kakao.maps.Map(mapContainer, options);
-                const markerPosition = new kakao.maps.LatLng(37.56000302825312, 126.97540593203321);
-                const marker = new kakao.maps.Marker({
-                    position: markerPosition
-                });
-                marker.setMap(map);
-            });
-        });
-
-        // var container = document.getElementById('map');
-        // var options = {
-        //     center: new kakao.maps.LatLng(37.365264512305174, 127.10676860117488),
-        //     level: 3
-        // };
-        // var map = new kakao.maps.Map(container, options);
-
-    }, []);
-
-    return (
-        <div className="Home">
-            <div id="map" className="map"></div>
-        </div>
-        // <div style={{ textAlign: "center" }}>
-
-        // </div>
-    );
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    //<div className='map' style={{width:"500px",height:"500px"}}> </div>
-    //const options = {
-    //  center: new window.kakao.maps.LatLng(33.450701, 126.570667),
-    // level :3,
-    //};
-    // function Home() {
-
-    //   function runMap() {
-    //     console.log('Maps JS API loaded');
-    //   const map = displayMap();
-    //}
-
-    // function loadMapsJSAPI(kakaoMapsAPIKeyt) {
-    //     const kakaoMapsAPIKeyt = 'API KEY';
-    //     const kakaoMapsAPIURL = 'https://dapi.kakao.com/v2/maps/sdk.js?appkey=658194b4f2b0f7fea8c9097f1a8b6b49';
-    // }
-
-
-    // const script = document.createElement('script');
-    // script.src = kakaoMapAPIURL;
-    // script.defer = true;
-    // script.async = true;
-
-    // window.runMap = runMap;
-    // document.head.appendChild(script);
-
-    // function displayMap() {
-    //     const mapOptions = {
-    //         center: { center: new kakao.maps.LatLng(37.56779, 126.97995) },
-    //         zoom: 14
-    //     };
-    // }
-    // <div id="map">
-    //     const mapDiv = document.getElementById ('map');
-    //     const map = new google.maps.Map(mapDiv, mapOptions);
-    //     return map;
-    // </div>
-
-
-
-};
 export default Home;
-
-
-
