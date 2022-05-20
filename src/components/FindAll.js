7435bb02acbc601d9fa6f784e92d75e3b3e3e31e
Yesol <violet010323@naver.com>
2022-05-20 17:58:37 +0900

    SidebarTest

--- src/components/FindAll.js
+++ src/components/FindAll.js
@@ -1,9 +1,20 @@
+import React from "react"; 
+// import Sidebar from "./components/Sidebar"
+// import "./Sidebar.css"
+
+
 const FindAll=()=>{
     return(
         <div style={{textAlign:"center"}}>
             FindAll
         </div>
     );
+    // { <div className="container">
+    //   <Sidebar />
+    //   </div> }
+  
 };
 
+
 export default FindAll;
+
