7dd954fd136e6877874391604dedcfcdb81b7eec
Yesol <violet010323@naver.com>
2022-05-20 19:24:31 +0900

    testt

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar"
 import Home from "./components/Home";
 import Login from "./components/Login.js";
 import { useState } from "react";
 import axios from "axios";
-import FindAll from "./components/FindAll";
-import FindHappy from "./components/FindHappy";
-import FindYoung from "./components/FindYoung";
-import FindRent from "./components/FindRent";
-import Sidebar from "./components/Sidebar";
 
 const App=()=>{
 
   const [data,setData]=useState(null);
   const onClick=()=>{
-    axios.get('/api/v1/houses').then(response=>{setData(response.data);});
+    axios.get('http://localhost:8080/api/v1/houses').then(response=>{setData(response.data);});
   };
 
   return(
@@ -23,17 +19,15 @@ const App=()=>{
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/Login" element={<Login />}/>
-        <Route path="/FindAll" element={<FindAll />}/>
-        <Route path="/FindYoung" element={<FindYoung />}/>
-        <Route path="/FindHappy" element={<FindHappy />}/>
-        <Route path="/FindRent" element={<FindRent />}/>
+        
       </Routes>
-      
+
       <div>
         <button onClick={onClick}>불러오기</button>
       </div>
       {data&&<textarea rows={7} value={JSON.stringify(data,null,2)} readOnly={true} />}
       <div className="container">
+      <Sidebar />
       </div>
     </div>
 
