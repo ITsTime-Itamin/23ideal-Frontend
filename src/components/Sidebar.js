e64503153ee3c454228a1b67fed29117dea9bb0c
chaeyeonan <dkscodus1204@ewhain.net>
2022-05-22 16:15:17 +0900

    chaeyeon: modify Sidebar.css

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,21 +3,21 @@ import "./Sidebar.css";
 
 const Sidebar=()=>{
     return(
-        <div>
-            <li>
+        <div className="sidebar">
+            <li className="sidebarTitle">
+                <Link to="/practice">모집공고 찾기</Link>
+            </li>
+            <li className="sidebarMenu">
                 <Link to="/practice">모집공고 전체보기</Link>
             </li>
-            <li>
+            <li className="sidebarMenu">
                 <Link to="/FindYoung">청년주택</Link>
             </li>
-            <li>
+            <li className="sidebarMenu">
                 <Link to="/FindHappy">행복주택</Link>
             </li>
-            <li>
+            <li className="sidebarMenu">
                 <Link to="/FindRent">임대주택</Link>
-            </li>
-            <li>
-                a음
             </li>
         </div>
     )
