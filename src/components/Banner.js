78e2707a534d47c8e033d3f9e79c159df63218da
Yesol <violet010323@naver.com>
2022-07-06 11:41:56 +0900

    yesol banner test

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,7 +5,7 @@ const Banner = () =>
  {
     return (
             <div className="Banner">
-                <img alt="서울주거포털" src="img/서울주거포털-02.png"> </img>
+                <img alt="Seoul" src="img/Seoul.png" ></img>
                 <img alt="서울청년포털" src="img/서울청년포털.png"> </img>
                 <img alt="경기주거복지포털" src="img/경기주거복지포털.png"> </img>
                 <img alt="서울주거상담" src="img/서울주거상담.png"> </img>
